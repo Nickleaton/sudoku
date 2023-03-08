0b6293017aa541afbdae1f307973fc8471fd5374
Nickleaton <nickle@gmail.com>
2023-03-08 12:46:52 +0000

    Mods to download script to ignore if already present. Changes of directory Logging Using argparse for parameters

--- scripts/create_fpuzzles_json.py
+++ scripts/create_fpuzzles_json.py
@@ -1,16 +1,24 @@
+import logging
+from argparse import ArgumentParser
 from urllib.parse import urlparse, parse_qs
 import lzstring
 import json
 from selenium import webdriver
 from pathlib import Path
 
-directory = "problems/fpuzzles/goodjson"
+# directory = "problems/fpuzzles/json"
+
+logging.basicConfig(level=logging.INFO)
 
 
-def process_url(url: str) -> None:
+def process_url(url: str, directory: Path) -> None:
     original_parsed = urlparse(url)
     original_qs = parse_qs(original_parsed.query)
     ident = original_qs['id'][0]
+    filename = Path(directory) / Path(ident + ".json")
+    if filename.exists():
+        logging.log(logging.INFO, f"Skipping file {filename.name}")
+        return
     driver = webdriver.Chrome()
     decompressor = lzstring.LZString()
     driver.get(url)
@@ -18,21 +26,34 @@ def process_url(url: str) -> None:
     query = parse_qs(current_parsed.query)
     uncompressed = decompressor.decompressFromBase64(query['load'][0].replace(" ", "+"))
     data = json.loads(uncompressed)
-    filename = Path(directory) / Path(ident + ".goodjson")
+
     all_data = {'url': url, 'data': data}
     with filename.open('w') as file:
+        logging.log(logging.INFO, f"Writing file {filename.name}")
         file.write(json.dumps(all_data, sort_keys=True, indent=4))
 
 
-def process_all() -> None:
-    print("Start")
-    with open('url.txt', 'r') as f:
+def get_parser() -> ArgumentParser:
+    result = ArgumentParser('create fpuzzles json', 'Down load json from an fpuzzles url file')
+    result.add_argument('-s', '--source')
+    result.add_argument('-d', '--destination')
+    result.add_argument('-v', '--verbose', action='store_true')
+    return result
+
+
+def main() -> None:
+    logging.log(logging.INFO, "Start")
+    parser = get_parser()
+    args = parser.parse_args()
+    source = Path(args.source)
+    destination = Path(args.destination)
+    with open(source, 'r') as f:
         urls = f.readlines()
     for i, u in enumerate(urls):
-        print(f"{i:04d} {len(urls):04d} {u.strip()}")
-        process_url(u.strip())
-    print("Finish")
+        logging.log(logging.INFO, f"{i:04d} {len(urls):04d} {u.strip()}")
+        process_url(u.strip(), destination)
+    logging.log(logging.INFO, "Finish")
 
 
 if __name__ == '__main__':
-    process_all()
+    main()
