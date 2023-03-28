baee7a79700d266e11bc403b4de7703d2028c8cf
Nickleaton <nickle@gmail.com>
2023-03-28 13:06:30 +0100

    remove os.path replacing with pathlib

--- tests/solver/test_formulation.py
+++ tests/solver/test_formulation.py
@@ -1,8 +1,9 @@
-import os
+import inspect
 import unittest
+from pathlib import Path
 from typing import List
 
-from pulp import LpVariable, LpInteger, LpProblem, LpMinimize, getSolver, LpSolver, LpContinuous
+from pulp import LpVariable, LpInteger, LpProblem, LpMinimize, getSolver, LpContinuous, LpSolver
 
 from src.solvers.formulations import Formulations
 
@@ -10,30 +11,31 @@ from src.solvers.formulations import Formulations
 class TestFormulation(unittest.TestCase):
 
     def setUp(self) -> None:
-        log_path = os.path.join("output", "formulations", "logs")
-        if not os.path.exists(log_path):  # pragma: no cover
-            os.makedirs(log_path)
-        lp_path = os.path.join("output", "formulations", "lp")
-        if not os.path.exists(lp_path):  # pragma: no cover
-            os.makedirs(lp_path)
+        self.base = Path("output/formulations")
+        self.log_path = self.base / Path("logs")
+        self.lp_path = self.base / Path("lp")
+        self.log_path.mkdir(exist_ok=True)
+        self.lp_path.mkdir(exist_ok=True)
 
-    @staticmethod
-    def get_application(name: str) -> LpSolver:
-        log_path = os.path.join("output", "formulations", "logs")
-        return getSolver('PULP_CBC_CMD', logPath=os.path.join(log_path, name + ".log"), msg=False, timeLimit=60)
+    def get_application(self, name: str) -> LpSolver:
+        log_file = self.log_path / Path(name + ".log")
+        return getSolver('PULP_CBC_CMD', logPath=str(log_file), msg=False, timeLimit=60)
+
+    def get_lp_filename(self) -> Path:
+        return self.lp_path / Path(inspect.stack()[1][3] + ".lp")
 
     def absolute_int(self, v1: int, v2: int, expected: int) -> None:
-        model = LpProblem("Sudoku", LpMinimize)
-        absolute = LpVariable("minimum", 0, 9, LpInteger)
+        model = LpProblem("absolute_int", LpMinimize)
+        x = LpVariable("minimum", 0, 9, LpInteger)
         x1 = LpVariable("x1", 1, 9, LpInteger)
         x2 = LpVariable("x2", 1, 9, LpInteger)
 
         model += x1 == v1
         model += x2 == v2
-        model += absolute == Formulations.abs(model, x1, x2, 9)
-        model.writeLP(os.path.join("output", "formulations", "lp", "absolute.lp"))
-        model.solve(TestFormulation.get_application('absolute'))
-        self.assertEqual(expected, absolute.varValue)
+        model += x == Formulations.abs(model, x1, x2, 9)
+        model.writeLP(str(self.get_lp_filename()))
+        model.solve(self.get_application('absolute'))
+        self.assertEqual(expected, x.varValue)
 
     def test_absolute_int(self) -> None:
         self.absolute_int(3, 9, 6)
@@ -41,17 +43,17 @@ class TestFormulation(unittest.TestCase):
         self.absolute_int(1, 1, 0)
 
     def absolute_float(self, v1: float, v2: float, expected: float) -> None:
-        model = LpProblem("Sudoku", LpMinimize)
-        absolute = LpVariable("minimum", 0, 9, LpContinuous)
+        model = LpProblem("absolute_float", LpMinimize)
+        x = LpVariable("minimum", 0, 9, LpContinuous)
         x1 = LpVariable("x1", 1, 9, LpContinuous)
         x2 = LpVariable("x2", 1, 9, LpContinuous)
 
         model += x1 == v1
         model += x2 == v2
-        model += absolute == Formulations.abs(model, x1, x2, 9)
-        model.writeLP(os.path.join("output", "formulations", "lp", "absolute.lp"))
-        model.solve(TestFormulation.get_application('absolute'))
-        self.assertEqual(expected, absolute.varValue)
+        model += x == Formulations.abs(model, x1, x2, 9)
+        model.writeLP(str(self.get_lp_filename()))
+        model.solve(self.get_application('absolute'))
+        self.assertEqual(expected, x.varValue)
 
     def test_absolute_float(self) -> None:
         self.absolute_float(3.0, 9.0, 6.0)
@@ -61,7 +63,7 @@ class TestFormulation(unittest.TestCase):
         self.absolute_float(4.5, 2.8, 1.7)
 
     def minimum(self, values: List[int], expected: int) -> None:
-        model = LpProblem("Sudoku", LpMinimize)
+        model = LpProblem("minimum", LpMinimize)
         variables: List[LpVariable] = []
         for i, value in enumerate(values):
             x = LpVariable(f"x{i}", 1, 9, LpInteger)
@@ -69,8 +71,8 @@ class TestFormulation(unittest.TestCase):
             variables.append(x)
 
         mini = Formulations.minimum(model, variables, 1, 9)
-        model.writeLP(os.path.join("output", "formulations", "lp", "minimum.lp"))
-        model.solve(TestFormulation.get_application('minimum'))
+        model.writeLP(str(self.get_lp_filename()))
+        model.solve(self.get_application('minimum'))
         self.assertEqual(expected, mini.varValue)
 
     def test_minimum(self) -> None:
@@ -79,7 +81,7 @@ class TestFormulation(unittest.TestCase):
         self.minimum([3, 2, 1], 1)
 
     def maximum(self, values: List[int], expected: int) -> None:
-        model = LpProblem("Sudoku", LpMinimize)
+        model = LpProblem("maximum", LpMinimize)
         variables: List[LpVariable] = []
         for i, value in enumerate(values):
             x = LpVariable(f"x{i}", 1, 9, LpInteger)
@@ -87,8 +89,8 @@ class TestFormulation(unittest.TestCase):
             variables.append(x)
 
         maxi = Formulations.maximum(model, variables, 1, 9)
-        model.writeLP(os.path.join("output", "formulations", "lp", "maximum.lp"))
-        model.solve(TestFormulation.get_application('maximum'))
+        model.writeLP(str(self.get_lp_filename()))
+        model.solve(self.get_application('maximum'))
         self.assertEqual(expected, maxi.varValue)
 
     def test_maximum(self) -> None:
@@ -97,12 +99,12 @@ class TestFormulation(unittest.TestCase):
         self.maximum([2, 2, 2], 2)
 
     def logical_not(self, value: int, expected: int) -> None:
-        model = LpProblem("Sudoku", LpMinimize)
+        model = LpProblem("logical_not", LpMinimize)
         x = LpVariable("x", 0, 1, LpInteger)
         model += x == value
         y = Formulations.logical_not(model, x)
-        model.writeLP(os.path.join("output", "formulations", "lp", "logical_not.lp"))
-        model.solve(TestFormulation.get_application('logical_not'))
+        model.writeLP(str(self.get_lp_filename()))
+        model.solve(self.get_application('logical_not'))
         self.assertEqual(expected, y.varValue)
 
     def test_logical_not(self):
@@ -110,14 +112,14 @@ class TestFormulation(unittest.TestCase):
         self.logical_not(0, 1)
 
     def logical_or(self, value1: int, value2: int, expected: int) -> None:
-        model = LpProblem("Sudoku", LpMinimize)
+        model = LpProblem("logical_or", LpMinimize)
         x1 = LpVariable("x1", 0, 1, LpInteger)
         x2 = LpVariable("x2", 0, 1, LpInteger)
         model += x1 == value1
         model += x2 == value2
         y = Formulations.logical_or(model, [x1, x2])
-        model.writeLP(os.path.join("output", "formulations", "lp", "logical_or.lp"))
-        model.solve(TestFormulation.get_application('logical_or'))
+        model.writeLP(str(self.get_lp_filename()))
+        model.solve(self.get_application('logical_or'))
         self.assertEqual(expected, y.varValue)
 
     def test_logical_or(self):
@@ -127,14 +129,15 @@ class TestFormulation(unittest.TestCase):
         self.logical_or(1, 1, 1)
 
     def logical_and(self, value1: int, value2: int, expected: int) -> None:
-        model = LpProblem("Sudoku", LpMinimize)
+
+        model = LpProblem("logical_and", LpMinimize)
         x1 = LpVariable("x1", 0, 1, LpInteger)
         x2 = LpVariable("x2", 0, 1, LpInteger)
         model += x1 == value1
         model += x2 == value2
         y = Formulations.logical_and(model, [x1, x2])
-        model.writeLP(os.path.join("output", "formulations", "lp", "logical_and.lp"))
-        model.solve(TestFormulation.get_application('logical_and'))
+        model.writeLP(str(self.get_lp_filename()))
+        model.solve(self.get_application('logical_and'))
         self.assertEqual(expected, y.varValue)
 
     def test_logical_and(self):
