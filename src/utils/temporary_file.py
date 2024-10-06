69efe280c4c2a134888079d0385abe1dc6b8b2d0
Nickleaton <nickle@gmail.com>
2024-10-06 22:45:54 +0100

    Comment up TemporaryFile

--- src/utils/temporary_file.py
+++ src/utils/temporary_file.py
@@ -1,3 +1,10 @@
+""" Generate a temporary filename in the temp directory
+
+        with TemporaryFile() as tf:
+            with open (tf.name, 'w') as f:
+                f.write ("Hello World")
+
+"""
 from pathlib import Path
 from uuid import uuid4
 
@@ -7,21 +14,31 @@ config = Config()
 
 
 class TemporaryFile:
+    """ Generate a temporary file name. Runs in a context """
 
     def __init__(self):
+        """  Create Temporary file
+
+        Use the config temporary directory specified in the config file
+        """
         directory = Path(config.temporary_directory)
         directory.mkdir(exist_ok=True)
         u = str(uuid4())
-        print(u)
         file_name = Path(u)
         self._name = directory / file_name
 
     @property
     def name(self) -> Path:
+        """ Get the name of the file
+
+        :return: Path
+        """
         return self._name
 
     def __enter__(self):
+        """ Handle the context """
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        """" Handle the context """
         self._name.unlink(missing_ok=True)
