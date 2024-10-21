633acdae79a9b4e6b052b78fea276a4b5790ad93
Nickleaton <nickle@gmail.com>
2024-10-21 18:34:13 +0100

    Add new problems

---
+++ acceptance_tests/test_problem090.py
@@ -0,0 +1,9 @@
+""" Acceptance test for problem problem090 """
+
+from tests.acceptance.acceptance_test import AcceptanceTest
+
+
+class TestProblem090(AcceptanceTest):
+
+    def setUp(self) -> None:
+        self.name = "problem090"
---
+++ acceptance_tests/test_problem091.py
@@ -0,0 +1,9 @@
+""" Acceptance test for problem problem091 """
+
+from tests.acceptance.acceptance_test import AcceptanceTest
+
+
+class TestProblem091(AcceptanceTest):
+
+    def setUp(self) -> None:
+        self.name = "problem091"
