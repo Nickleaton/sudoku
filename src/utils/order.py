16fc93f5b62d4a78ecf0d7f40c8adc9a94c658f7
Nickleaton <nickle@gmail.com>
2024-10-22 16:21:34 +0100

    add unordered for AntiRossini

--- src/utils/order.py
+++ src/utils/order.py
@@ -1,36 +1,27 @@
 from enum import Enum
 
 
-class OrderException(Exception):
-    """Exceptions generated during Order Handling"""
-
-
 class Order(Enum):
     INCREASING = 'I'
     DECREASING = 'D'
+    UNORDERED = 'U'
 
     def __neg__(self) -> 'Order':
-        if self == Order.INCREASING:
-            return Order.DECREASING
-        if self == Order.DECREASING:
-            return Order.INCREASING
-        raise OrderException('Unknown Order')  # pragma: no cover
-
-    @staticmethod
-    def create(letter: str) -> 'Order':
-        if letter == 'I':
-            return Order.INCREASING
-        if letter == 'D':
-            return Order.DECREASING
-        raise OrderException(f"Unknown order letter {letter}")
+        negation_map = {
+            Order.INCREASING: Order.DECREASING,
+            Order.DECREASING: Order.INCREASING,
+            Order.UNORDERED: Order.UNORDERED
+        }
+        return negation_map[self]
 
     @staticmethod
     def valid(letter: str) -> bool:
-        return letter in ['I', 'D']
+        return letter in Order.values()
 
     @staticmethod
     def values() -> str:
-        return "".join([order.value for order in Order])
+        return "".join(order.value for order in Order)
 
     def __repr__(self) -> str:
         return f"Order.{self.name}"
+
--- tests/utils/test_order.py
+++ tests/utils/test_order.py
@@ -1,28 +1,32 @@
 import unittest
 
-from src.utils.order import Order, OrderException
+from src.utils.order import Order
 
 
 class TestOrder(unittest.TestCase):
 
     def test_create(self):
-        self.assertEqual(Order.INCREASING, Order.create("I"))
-        self.assertEqual(Order.DECREASING, Order.create("D"))
-        with self.assertRaises(OrderException):
-            Order.create('X')
+        self.assertEqual(Order.INCREASING, Order("I"))
+        self.assertEqual(Order.DECREASING, Order("D"))
+        self.assertEqual(Order.UNORDERED, Order("U"))
+        with self.assertRaises(ValueError):
+            Order('X')
 
     def test_valid(self):
         self.assertTrue(Order.valid('I'))
         self.assertTrue(Order.valid('D'))
+        self.assertTrue(Order.valid('U'))
         self.assertFalse(Order.valid('X'))
 
     def test_repr(self):
         self.assertEqual('Order.INCREASING', repr(Order.INCREASING))
         self.assertEqual('Order.DECREASING', repr(Order.DECREASING))
+        self.assertEqual('Order.UNORDERED', repr(Order.UNORDERED))
 
     def test_negate(self):
         self.assertEqual(Order.INCREASING, - Order.DECREASING)
         self.assertEqual(Order.DECREASING, - Order.INCREASING)
+        self.assertEqual(Order.UNORDERED, - Order.UNORDERED)
 
 
 if __name__ == '__main__':  # pragma: no cover
