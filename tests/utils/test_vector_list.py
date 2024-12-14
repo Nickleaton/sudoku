8816464cbf52bb72b0f756eafb4fc57831d7f341
Nickleaton <nickle@gmail.com>
2024-12-14 16:20:39 +0000

    Flake 8 fixed and tests pass

--- tests/utils/test_vector_list.py
+++ tests/utils/test_vector_list.py
@@ -11,7 +11,7 @@ class TestVectorList(unittest.TestCase):
     """Test suite for the VectorList class."""
 
     def setUp(self) -> None:
-        """Initialize test data."""
+        """Initialize test input_data."""
         self.vectors1 = VectorList(
             [
                 Vector(Coord(1, 2), Coord(2, 3)),
