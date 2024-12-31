7456adeee75a0fc6efcbd32386be008a9c4bf230
Nickleaton <nickle@gmail.com>
2024-12-31 00:58:27 +0000

    flake8

--- src/validators/validator.py
+++ src/validators/validator.py
@@ -1,30 +1,30 @@
-"""Validator."""
+"""Validator Module."""
 from src.board.board import Board
 
 
 class Validator:
     """Base class for board validation.
 
-    This class provides start base structure for validators that check
-    the validity of start `Board` against some input_data. It can be extended
+    This class provides a base structure for validators that check
+    the validity of the start `Board` against some `input_data`. It can be extended
     by custom validators for specific types of validation.
     """
 
     @staticmethod
-    def validate_keys(input_data: dict, required_keys: list[str]) -> list[str]:
+    def validate_keys(input_data: dict, required_keys: list) -> list:
         """Validate the required keys in the input_data dictionary.
 
         Args:
             input_data (dict): The input_data dictionary to validate.
-            required_keys (list[str]): The list of keys that must be present in the input_data.
+            required_keys (list): The list of keys that must be present in the input_data.
 
         Returns:
-            list[str]: A list of error messages if any required keys are missing.
+            list: A list of error messages if any required keys are missing.
         """
         return [f'Missing key: "{key}"' for key in required_keys if key not in input_data]
 
     @staticmethod
-    def validate(board: Board, input_data: dict) -> list[str]:
+    def validate(board: Board, input_data: dict) -> list:
         """Validate the provided input_data dictionary against the board.
 
         This method is intended to be overridden by subclasses to
@@ -36,13 +36,13 @@ class Validator:
             input_data (dict): The input_data to validate.
 
         Returns:
-            list[str]: A list of error messages. If no validation errors
+            list: A list of error messages. If no validation errors
             are found, an empty list is returned.
         """
         return []
 
     def __repr__(self) -> str:
-        """Return start string representation of the Validator class.
+        """Return string representation of the Validator class.
 
         Returns:
             str: A string representation of the class, including its name.
