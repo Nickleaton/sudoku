81c97042d702aa51a490f7686d4e3dca39500c77
Nickleaton <nickle@gmail.com>
2025-01-15 09:23:43 +0000

    Test of coord

--- tests/utils/test_coord.py
+++ tests/utils/test_coord.py
@@ -11,8 +11,9 @@ class TestCoord(unittest.TestCase):
         self.coord_a = Coord(2, 3)
         self.coord_b = Coord(5, 7)
         self.coord_c = Coord(-3, -4)
-        self.tolerance = 1e-9
 
+
+Te
     def test_addition(self):
         """Test the addition of two Coord objects."""
         result = self.coord_a + self.coord_b
@@ -34,39 +35,24 @@ class TestCoord(unittest.TestCase):
         with self.assertRaises(CoordException):
             _ = self.coord_a * "invalid"
 
-    def test_division(self):
-        """Test the division of Coord by a scalar."""
-        result = self.coord_b / 2
-        self.assertEqual(result, Coord(2.5, 3.5))
-
     def test_negation(self):
         """Test negation of a Coord object."""
         result = -self.coord_a
         self.assertEqual(result, Coord(-2, -3))
 
-    def test_equality(self):
-        """Test equality comparison with tolerance."""
-        coord_d = Coord(2.000000001, 3.000000001)
-        self.assertTrue(self.coord_a == coord_d)
-
-        coord_e = Coord(2.1, 3.1)
-        self.assertFalse(self.coord_a == coord_e)
-
     def test_parallel(self):
-        # Test directions that are almost parallel but slightly off (within tolerance)
-        direction_a = Coord(1.0, 1.0)  # Close to 45 angle_degree (diagonal)
-        direction_b = Coord(1.0, 1.000000001)  # Slightly off, but still parallel
+        # Test directions that are almost parallel
+        direction_a = Coord(1, 1)
+        direction_b = Coord(1, 1)
+        direction_c = Coord(1, 9)
         self.assertTrue(direction_a.parallel(direction_b))
+        self.assertFalse(direction_a.parallel(direction_c))
 
     def test_comparison(self):
         """Test less-than comparison of Coord objects."""
         self.assertTrue(self.coord_a < self.coord_b)
         self.assertFalse(self.coord_b < self.coord_a)
 
-    def test_middle(self):
-        """Test the calculation of the midpoint between two Coord objects."""
-        result = Coord.middle(self.coord_a, self.coord_b)
-        self.assertEqual(result, Coord(3.5, 5))
 
     def test_create_from_int(self):
         """Test creation of Coord from an integer representation."""
