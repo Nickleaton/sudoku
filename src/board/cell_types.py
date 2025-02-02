01de0db93a7efecb69642533e1c5cf074829b16b
Nickleaton <nickle@gmail.com>
2025-02-02 10:41:54 +0000

    mypy no type annotation fixed

--- src/board/cell_types.py
+++ src/board/cell_types.py
@@ -5,28 +5,28 @@ from enum import IntEnum
 class ParityType(IntEnum):
     """Represents the parity of a number."""
 
-    odd: int = 0
-    even: int = 1
+    odd = 0
+    even = 1
 
 
 class ModuloType(IntEnum):
     """Represents modulo classifications."""
 
-    mod0: int = 0
-    mod1: int = 1
-    mod2: int = 2
+    mod0 = 0
+    mod1 = 1
+    mod2 = 2
 
 
 class PrimeType(IntEnum):
     """Represents whether a number is prime or composite."""
 
-    prime: int = 0
-    composite: int = 1
+    prime = 0
+    composite = 1
 
 
 class EntropicType(IntEnum):
     """Represents entropy levels."""
 
-    low: int = 0
-    medium: int = 1
-    high: int = 2
+    low = 0
+    medium = 1
+    high = 2
