52ac1655298c59696f103481af5d3ae806299d4f
Nickleaton <nickle@gmail.com>
2025-02-04 22:49:09 +0000

    fixed ChatGPT generated code

--- tests/utils/test_direction.py
+++ tests/utils/test_direction.py
@@ -11,7 +11,7 @@ class TestDirection(unittest.TestCase):
         """Test creating Direction instances from string input."""
         self.assertEqual(Direction.up_left, Direction.create("UL"))
         self.assertEqual(Direction.up, Direction.create("U"))
-        self.assertEqual(Direction.DOWN_RIGHT, Direction.create("DR"))
+        self.assertEqual(Direction.down_right, Direction.create("DR"))
         with self.assertRaises(ValueError):
             Direction.create('XX')
 
@@ -19,15 +19,15 @@ class TestDirection(unittest.TestCase):
         """Test the coordinate offset for each Direction."""
         self.assertEqual(Direction.up_left.coord, Coord(-1, -1))
         self.assertEqual(Direction.up.coord, Coord(-1, 0))
-        self.assertEqual(Direction.RIGHT.coord, Coord(0, 1))
-        self.assertEqual(Direction.DOWN.coord, Coord(1, 0))
-        self.assertEqual(Direction.DOWN_RIGHT.coord, Coord(1, 1))
+        self.assertEqual(Direction.right.coord, Coord(0, 1))
+        self.assertEqual(Direction.down.coord, Coord(1, 0))
+        self.assertEqual(Direction.down_right.coord, Coord(1, 1))
 
     def test_repr(self):
         """Test the string representation of Direction instances."""
         self.assertEqual('Direction.up_left', repr(Direction.up_left))
         self.assertEqual('Direction.up', repr(Direction.up))
-        self.assertEqual('Direction.DOWN_RIGHT', repr(Direction.DOWN_RIGHT))
+        self.assertEqual('Direction.down_right', repr(Direction.down_right))
 
 
 if __name__ == '__main__':  # pragma: no cover
