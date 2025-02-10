f28567ba4bfc00c1a3de1f77f3cb9546bb0219f7
Nickleaton <nickle@gmail.com>
2025-02-10 23:00:06 +0000

    update build for generating documnetation

--- scripts/generate_documentation.py
+++ scripts/generate_documentation.py
@@ -5,6 +5,8 @@ import logging.config
 import sys
 from pathlib import Path
 
+from jinja2 import Environment, FileSystemLoader
+
 from src.items.item import Item
 from src.utils.config import Config
 from src.utils.load_modules import load_modules
@@ -13,8 +15,10 @@ config: Config = Config()
 logging.config.dictConfig(Config().get_dict('logging'))
 logger = logging.getLogger("generate_documentation")
 
+env = Environment(loader=FileSystemLoader(Path('src/templates')), autoescape=True)
 
-def process_file(clazz: type[Item], file_path: Path) -> None:
+
+def process_file(clazz: type[Item], file_path: Path, template_name: str) -> None:
     """Create Documentation
 
     Args:
@@ -25,6 +29,12 @@ def process_file(clazz: type[Item], file_path: Path) -> None:
         None
     """
     logging.info(f"Processing {clazz.__name__} -> {file_path}")
+    if not file_path.parent.exists():
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+    template = env.get_template(template_name)
+    info: dict[str, str] = clazz.get_info()
+    output: str = template.render(info=info)
+    file_path.write_text(output, encoding='utf-8')
 
 
 def document(target_directory: Path) -> None:
@@ -41,8 +51,10 @@ def document(target_directory: Path) -> None:
     for name, clazz in Item.classes.items():
         if name == 'Solution':
             continue
-        file_path: Path = target_directory / (name + '.rst')
-        process_file(clazz, file_path)
+        file_path: Path = target_directory / Path('rst') / (name + '.rst')
+        process_file(clazz, file_path, 'documentation.rst')
+        file_path: Path = target_directory / Path('md') / (name + '.md')
+        process_file(clazz, file_path, 'documentation.md')
 
 
 def create_arg_parser() -> argparse.ArgumentParser:
